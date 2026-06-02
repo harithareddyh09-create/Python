d1e368f0f3ebe28bce83f2e1aab45fa2aa0a99b6
Harithareddy <harithareddyh09@gmail.com>
2026-06-02 19:33:19 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,7 @@
+print("---------- E-commerce cart-------------")
+name=str(input("Enter your Name:"))
+quantity=int(input("Enter quantity of items:"))
+price=float(input("Enter price of one item:"))
+total_price=quantity*price
+print("Total price of items:",total_price)
+print("Thank you for shopping with us..!")
